01e311e0469b2d09c6d96419763bc8b1a731070e
AfrinMahammad <95007495+AfrinMahammad@users.noreply.github.com>
2022-08-13 19:43:06 +0530

    Time: 324 ms (60.90%), Space: 0B (100.00%) - LeetHub

--- 596-classes-more-than-5-students/596-classes-more-than-5-students.sql
+++ 596-classes-more-than-5-students/596-classes-more-than-5-students.sql
@@ -1,5 +1,4 @@
 # Write your MySQL query statement below
 select class from 
-(select count(student),class
- from courses group by 
- class having count(student)>=5) i;
+ courses group by 
+ class having count(student)>=5;
