bd6ee6dd0eac8c9a6c023e90bcaae99900ea0426
AfrinMahammad <95007495+AfrinMahammad@users.noreply.github.com>
2022-08-14 20:14:24 +0530

    Time: 732 ms (73.04%), Space: 0B (100.00%) - LeetHub

--- 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
@@ -1,2 +1,2 @@
 /* Write your PL/SQL query statement below */
-select user_id,initcap(name) name from users order by user_id;
+select user_id,concat(upper(substr(name,1,1)),lower(substr(name,2)))name from users order by user_id;
