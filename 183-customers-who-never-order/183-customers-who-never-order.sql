90850c4356bd59dcc8f386c4e8819885552adf69
AfrinMahammad <95007495+AfrinMahammad@users.noreply.github.com>
2022-08-15 12:25:30 +0530

    Time: 681 ms (95.37%), Space: 0B (100.00%) - LeetHub

--- 183-customers-who-never-order/183-customers-who-never-order.sql
+++ 183-customers-who-never-order/183-customers-who-never-order.sql
@@ -1,2 +1,2 @@
-# Write your MySQL query statement below
+/* Write your PL/SQL query statement below */
 select name customers from customers where id not in (select distinct customerId from orders);
