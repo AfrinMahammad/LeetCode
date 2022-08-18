fb15c629ec677041c0e08f1a210b5952745325a7
AfrinMahammad <95007495+AfrinMahammad@users.noreply.github.com>
2022-08-18 22:54:29 +0530

    Time: 691 ms (69.49%), Space: 0B (100.00%) - LeetHub

---
+++ 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
@@ -0,0 +1,2 @@
+/* Write your PL/SQL query statement below */
+select to_char(activity_date,'yyyy-mm-dd') day, count(distinct user_id) active_users from activity where activity_date between '2019-06-28' and '2019-07-27' group by to_char(activity_date,'yyyy-mm-dd');
