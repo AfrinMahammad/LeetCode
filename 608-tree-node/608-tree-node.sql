72a813fa88795167edb3490b386b0226a06f9caf
AfrinMahammad <95007495+AfrinMahammad@users.noreply.github.com>
2022-08-19 17:44:38 +0530

    Time: 617 ms (97.21%), Space: 0B (100.00%) - LeetHub

--- 608-tree-node/608-tree-node.sql
+++ 608-tree-node/608-tree-node.sql
@@ -1,9 +1,9 @@
-# Write your MySQL query statement below
+/* Write your PL/SQL query statement below */
 select id,
 case
     when p_id is null then 'Root'
-    when id in (select distinct p_id from Tree) then 'Inner'
+    when id in (select distinct(p_id) from Tree) then 'Inner'
     else 'Leaf' 
-end as 'type'
+end as type
 from Tree
 order by id asc;
