53602bc9d75db52d62301dc30e4d2cc705074d16
AfrinMahammad <95007495+AfrinMahammad@users.noreply.github.com>
2022-08-24 22:19:11 +0530

    Time: 54 ms (72.75%), Space: 13.8 MB (98.29%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.py
+++ 43-multiply-strings/43-multiply-strings.py
@@ -1,3 +1,19 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        return str(int(num1)*int(num2))
+        i,j=0,0
+        res=0
+        if num1=='0' or num2=='0':
+            return '0'
+        elif num1=='1':
+            return num2
+        elif num2=='1':
+            return num1
+        else:
+            n=int(num1)
+            i,p=len(num2)-1,1
+            while i>=0:
+                t=int(num2[i])*n
+                res=res+p*t
+                p=p*10
+                i-=1
+            return str(res)
