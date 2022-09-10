e2976e8feaac6a04f25f2925955d7cfbf90ed7c7
AfrinMahammad <95007495+AfrinMahammad@users.noreply.github.com>
2022-09-10 16:01:36 +0530

    Time: 113 ms (20.43%), Space: 13.8 MB (94.57%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.py
+++ 692-top-k-frequent-words/692-top-k-frequent-words.py
@@ -1,17 +1,11 @@
 from collections import OrderedDict
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        res=[]
         d={}
         for i in words:
             if i not in d:
                 d[i]=1
             else:
                 d[i]+=1
-        values=sorted(list(d.values()),reverse=True)
-        d=OrderedDict(sorted(d.items()))
-        for i in range(0,k):
-            res.append(list(d.keys())[list(d.values()).index(values[i])])
-            d.pop(res[-1])
-        return res    
-        
+        res=sorted(d,key=lambda x:(-d[x],x))
+        return res[:k]
